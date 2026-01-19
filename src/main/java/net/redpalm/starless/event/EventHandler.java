0d82022d5cb470abb698e0d7ef7f540de985fecc
Red Palm <mathanalysis123@gmail.com>
2026-01-20 00:55:08 +0300

    Spawn timers bugfixes

--- src/main/java/net/redpalm/starless/event/EventHandler.java
+++ src/main/java/net/redpalm/starless/event/EventHandler.java
@@ -12,7 +12,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.event.level.SleepFinishedTimeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -27,11 +26,10 @@ import static net.redpalm.starless.misc.WrongedItemList.wrongedItemList;
 
 @Mod.EventBusSubscriber(modid = Starless.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class EventHandler extends Event {
-    static Random random = new Random();
     static int randomIndex;
     static long lastSpawnWronged = 0;
     static long lastSpawnObserve = 0;
-    static int clockWronged = 0;
+    static Random random = new Random();
 
     // Attempt to spawn Observe after some amount of ticks with 10% chance
     @SubscribeEvent
@@ -46,34 +44,20 @@ public class EventHandler extends Event {
         int spawnTime = 8000;
         int spawnChance = 10;
 
-        if (tick.level.getDayTime() % spawnTime == 0 && random.nextInt(spawnChance) == 0 &&
-        tick.level.getDayTime() != lastSpawnObserve) {
+        if ((tick.level.getGameTime() % spawnTime == 0) && tick.level.getRandom().nextInt(spawnChance) == 0 &&
+        tick.level.getGameTime() != lastSpawnObserve) {
 
-            lastSpawnObserve = tick.level.getDayTime();
+            lastSpawnObserve = tick.level.getGameTime();
             ObserveEntity entity = ModEntities.OBSERVE.get().create(tick.level);
             if (entity == null) return;
 
             Player player = tick.level.getServer().getPlayerList().getPlayers().get
-                    (random.nextInt(tick.level.getServer().getPlayerList().getPlayers().size()));
+                    (tick.level.getRandom().nextInt(tick.level.getServer().getPlayerList().getPlayers().size()));
 
             spawnEntity(10, 10, entity, player, tick);
 
             tick.level.playSound(null, entity.getX(), entity.getY(), entity.getZ(),
                     SoundEvents.AMBIENT_CAVE.get(), SoundSource.HOSTILE, 2f, 0.85f);
-        }
-    }
-
-    // Clock for Wronged
-    @SubscribeEvent
-    public static void clockWronged(TickEvent.LevelTickEvent tick) {
-        if (tick.phase != TickEvent.Phase.END) return;
-        if (!(tick.level instanceof ServerLevel)) return;
-        if (tick.level.isClientSide) return;
-        if (tick.level.dimension() != Level.OVERWORLD) return;
-
-        clockWronged++;
-        if ((tick.level.getDayTime() % 24000) == 0) {
-            clockWronged = 0;
         }
     }
 
@@ -91,27 +75,27 @@ public class EventHandler extends Event {
         int chancePhrase = 3;
         int Midnight = 18000;
 
-        if (((clockWronged % Midnight) == 0 || (tick.level.getDayTime() == Midnight))  &&
-                random.nextInt(chanceSpawn) == 0 && tick.level.getDayTime() != lastSpawnWronged) {
+        if (((tick.level.getGameTime() % 24000 == Midnight) || tick.level.getGameTime() == Midnight)  &&
+                tick.level.getRandom().nextInt(chanceSpawn) == 0 && tick.level.getGameTime() != lastSpawnWronged) {
 
-            lastSpawnWronged = tick.level.getDayTime();
+            lastSpawnWronged = tick.level.getGameTime();
             WrongedEntity entity = ModEntities.WRONGED.get().create(tick.level);
             if (entity == null) return;
 
             Player player = tick.level.getServer().getPlayerList().getPlayers().get
-                    (random.nextInt(tick.level.getServer().getPlayerList().getPlayers().size()));
+                    (tick.level.getRandom().nextInt(tick.level.getServer().getPlayerList().getPlayers().size()));
 
             spawnEntity(10, 10, entity, player, tick);
 
             tick.level.getServer().getPlayerList().broadcastSystemMessage
                     (Component.literal("<Wrong.ed> Hello."), false);
 
-            if (random.nextInt(chancePhrase) == 0) {
+            if (tick.level.getRandom().nextInt(chancePhrase) == 0) {
                 tick.level.getServer().getPlayerList().broadcastSystemMessage
                         (Component.literal("<Wrong.ed> Can I give you something? " +
                                 "I hope it's okay."), false);
             }
-            else if (random.nextInt(chancePhrase) == 1) {
+            else if (tick.level.getRandom().nextInt(chancePhrase) == 1) {
                 tick.level.getServer().getPlayerList().broadcastSystemMessage
                         (Component.literal("<Wrong.ed> I can give you an item. " +
                                 "Not sure if it's as useful as I think it is."), false);
@@ -125,23 +109,18 @@ public class EventHandler extends Event {
         }
     }
 
-    @SubscribeEvent
-    public static void onPlayerWake(SleepFinishedTimeEvent event) {
-        clockWronged = 0;
-    }
-
     public static void spawnEntity(int extraX, int extraZ, LivingEntity entity,
                                    Player player, TickEvent.LevelTickEvent event) {
         int entityX;
         int entityZ;
         int chance = 2;
-        if (random.nextInt(chance) == 0) {
-            entityX = (int) player.getX() + random.nextInt(15) + extraX;
-            entityZ = (int) player.getZ() + random.nextInt(15) + extraZ;
+        if (event.level.getRandom().nextInt(chance) == 0) {
+            entityX = (int) player.getX() + event.level.getRandom().nextInt(15) + extraX;
+            entityZ = (int) player.getZ() + event.level.getRandom().nextInt(15) + extraZ;
         }
         else {
-            entityX = (int) player.getX() - random.nextInt(15) - extraX;
-            entityZ = (int) player.getZ() - random.nextInt(15) - extraZ;
+            entityX = (int) player.getX() - event.level.getRandom().nextInt(15) - extraX;
+            entityZ = (int) player.getZ() - event.level.getRandom().nextInt(15) - extraZ;
         }
         entity.setPos(entityX, event.level.getHeight(Heightmap.Types.WORLD_SURFACE,
                 entityX, entityZ), entityZ);
